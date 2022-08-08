6982e850667b253b1c7cde024f5458da91d04957
ZikaZaki <zika.github@gmail.com>
2022-08-08 19:01:09 +0300

    Add The createSpeakers() Function to JavaScript File

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,110 @@
+//= ==========================SPEAKERS-CARDS DATA==================================//
+const speakersData = [
+  {
+    id: 1,
+    name: 'ZikaZaki',    
+    image: '/assets/img/speaker1.png',
+    organization: 'MIT Institute',
+    description:
+    'Focusing on a collaborative approach in a networked environment, '
+    + 'we created the concept of co-production based on sharing, '
+    + 'such as open source software and Wikipedia.',
+  },
+  {
+   id: 2,
+   name: 'ZikaZaki',    
+   image: '/assets/img/speaker2.png',
+   organization: 'MIT Institute',
+   description:
+    'Focusing on a collaborative approach in a networked environment, '
+    + 'we created the concept of co-production based on sharing, '
+    + 'such as open source software and Wikipedia.',
+  },
+  {
+    id: 3,
+    name: 'ZikaZaki',    
+    image: '/assets/img/speaker3.png',
+    organization: 'MIT Institute',
+    description:
+    'Focusing on a collaborative approach in a networked environment, '
+    + 'we created the concept of co-production based on sharing, '
+    + 'such as open source software and Wikipedia.',
+  },
+  {
+    id: 4,
+    name: 'ZikaZaki',    
+    image: '/assets/img/speaker4.png',
+    organization: 'MIT Institute',
+    description:
+    'Focusing on a collaborative approach in a networked environment, '
+    + 'we created the concept of co-production based on sharing, '
+    + 'such as open source software and Wikipedia.',
+  },
+  {
+    id: 5,
+    name: 'ZikaZaki',    
+    image: '/assets/img/speaker5.png',
+    organization: 'MIT Institute',
+    description:
+    'Focusing on a collaborative approach in a networked environment, '
+    + 'we created the concept of co-production based on sharing, '
+    + 'such as open source software and Wikipedia.',
+  },
+  {
+    id: 6,
+    name: 'ZikaZaki',    
+    image: '/assets/img/speaker6.png',
+    organization: 'MIT Institute',
+    description:
+      'Focusing on a collaborative approach in a networked environment, '
+      + 'we created the concept of co-production based on sharing, '
+      + 'such as open source software and Wikipedia.',
+  },
+];
+
+//= =====================CREATE SPEAKERS CARD FUNCTION========================//
+function createSpeakersCard(obj) {
+  // Speaker image
+  const speaker = document.createElement('div');
+  speaker.classList.add('speaker');
+  const speakerImage = document.createElement('div');
+  speakerImage.classList.add('speaker-image');
+  const speakerImageImg = document.createElement('img');
+  speakerImageImg.src = obj.image;
+  speakerImage.appendChild(speakerImageImg);
+  speaker.appendChild(speakerImage);
+  // Speaker info
+  const speakerInfo = document.createElement('div');
+  speakerInfo.classList.add('speaker-info');
+  const speakerName = document.createElement('div');
+  speakerName.classList.add('speaker-name');
+  const speakerNameLink = document.createElement('a');
+  speakerNameLink.href = '#';
+  speakerNameLink.innerText = obj.name;
+  speakerName.appendChild(speakerNameLink);
+  speakerInfo.appendChild(speakerName);
+
+  // Speaker org
+  const speakerOrg = document.createElement('div');
+  speakerOrg.classList.add('org-name');
+  speakerOrg.innerText = obj.organization;
+  speakerInfo.appendChild(speakerOrg);
+
+  // Speaker desc
+  const speakerDesc = document.createElement('div');
+  speakerDesc.classList.add('speaker-desc');
+  speakerDesc.innerText = obj.description;
+  speakerInfo.appendChild(speakerDesc);
+
+  speaker.appendChild(speakerInfo);
+  return speaker;
+}
+
+const speakersList = document.querySelector('.speakers-list');
+speakersData.forEach(obj => {
+  speakersList.appendChild(createSpeakersCard(obj));
+});
+
 /* Toggle Button Function Starts Here */
 const sideMenu = document.getElementById('side_menu');
 const sideMenuNav = document.getElementById('side_menu_nav');
